855575c198a942742a37be4f73c70f693af3ba77
Gerald Gehrke <TripleG58@users.noreply.github.com>
2023-06-04 11:58:01 -0500

    Update DBConstruction.js

--- DBConstruction.js
+++ DBConstruction.js
@@ -1,1 +1,36 @@
+// download armor.csv from DIM "organizer"
+// convert csv to json using devkit
+// store json in "csv" variable
 
+// parameters //
+
+let warlock = {
+  "goals": {
+    "res": 10,
+    "rec": 20,
+    "disc": 23,
+    "str": 23
+  },
+  "sets": {
+    "disc": {
+      "goals": []
+    }
+  } 
+}
+
+// stat goals
+// warlock
+let w_res = 10;
+let w_rec = 20;
+let w_disc = 23;
+let w_str = 23;
+// titan
+let t_res = 20;
+let t_rec = 10;
+let t_disc = 23;
+let t_str = 23;
+// hunter
+let h_mob = 30;
+let h_res = 30;
+let h_rec = 30;
+let h_disc = 23;
