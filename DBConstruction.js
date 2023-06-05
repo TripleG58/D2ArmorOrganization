0ecb7aea9cb1c3652f7da98f16151d612b34e81c
Gerald Gehrke <TripleG58@users.noreply.github.com>
2023-06-05 12:53:22 -0500

    Update DBConstruction.js

--- DBConstruction.js
+++ DBConstruction.js
@@ -2,9 +2,7 @@
 // convert csv to json using devkit
 // store json in "csv" variable
 
-// parameters //
-
-// stat goals
+// stat goals //
 // warlock
 let w_res = 10;
 let w_rec = 20;
@@ -38,10 +36,113 @@ legend = legend.filter(item => item.Tag !== "junk");
 legend = legend.filter(item => item.Tag !== "infuse");
 legend = legend.filter(item => item.Tag !== "");
 
+function buildDefecitsAndSupplementsHunter(obj, itemMob, itemRes, itemRec, itemDisc, mobGoal, resGoal, recGoal, discGoal) {
+  let d_diff = itemDisc - discGoal;
+  let mob_diff = itemMob - mobGoal;
+  let res_diff = itemRes - resGoal;
+  let rec_diff = itemRec - recGoal;
+  obj.spike = "mob";
+  if (itemRes > itemMob) {
+    obj.spike = "res";
+  }
+  if (itemRec > itemMob && itemRec > itemRes) {
+    obj.spike = "rec";
+  }
+  switch(obj.spike) {
+    default;
+      break;
+    case "mob":
+      if (mob_diff > 0) {
+        obj.supplements += "mob +" + mob_diff + ", ";
+      }
+      else if (mob_diff < 0) {
+        obj.defecits += "mob " + mob_diff + ", ";
+      }
+      break;
+    case "res":
+      if (res_diff > 0) {
+        obj.supplements += "res +" + res_diff + ", ";
+      }
+      else if (res_diff < 0) {
+        obj.defecits += "res " + res_diff + ", ";
+      }
+      break;
+    case "rec":
+      if (rec_diff > 0) {
+        obj.supplements += "rec +" + rec_diff + ", ";
+      }
+      else if (rec_diff < 0) {
+        obj.defecits += "rec " + rec_diff + ", ";
+      }
+      break;
+  }
+  if (d_diff > 0) {
+    obj.supplements += "disc +" + d_diff + ", ";
+  }
+  else if (d_diff < 0) {
+    obj.defecits += "disc " + d_diff + ", ";
+  }
+}
+
+function buildDefecitsAndSupplementsWarlockAndTitan(obj, itemRes, itemRec, itemDisc, itemStr, resGoal, recGoal, discGoal, strGoal) {
+  // compare stats and compile defecit/supplement strings and "spike"
+  obj.spike = (itemDisc >= itemStr) ? "disc" : "str";
+  let d_diff = itemDisc - discGoal;
+  let s_diff = itemStr - strGoal;
+  let res_diff = itemRes - resGoal;
+  let rec_diff = itemRec - recGoal;
+  // resilience defecits/supplements
+  if (res_diff > 0) {
+    obj.supplements += "res +" + res_diff + ", ";
+  }
+  else if (res_diff < 0) {
+    obj.defecits += "res " + res_diff + ", ";
+  }
+  // recovery defecits/supplements
+  if (rec_diff > 0) {
+    obj.supplements += "rec +" + rec_diff + ", ";
+  }
+  else if (rec_diff < 0) {
+    obj.defecits += "rec " + rec_diff + ", ";
+  }
+  // discipline & strength defecits/supplements depending on spike
+  if (obj.spike === "disc") {
+    if (d_diff > 0) {
+      obj.supplements += "disc +" + d_diff + ", ";
+    }
+    else if (d_diff < 0) {
+      obj.defecits += "disc " + d_diff + ", ";
+    }
+  }
+  else if (obj.spike === "str") {
+    if (s_diff > 0) {
+      obj.supplements += "str +" + s_diff + ", ";
+    }
+    else if (s_diff < 0) {
+      obj.defecits += "str " + s_diff + ", ";
+    }
+  }
+  // determine if supplementing for subclass fragments (10 stat)
+  if (obj.class === "Warlock" && res_diff >= 10) {
+    obj.spike = "res/" + obj.spike;
+  }
+  else if (obj.class === "Titan" && rec_diff >= 10) {
+    obj.spike = "rec/" + obj.spike;
+  }
+}
+
 for (let i = 0; i < legend.length; ++i) {
-  let obj = {};
-  obj.type = legend[i].Type // "slot"
-  obj.raid = legend[i].Soure 
+  let obj = {
+    "id": "",
+    "class": "",
+    "type": "",
+    "raid": "",
+    "spike": "",
+    "defecits": "",
+    "supplements": ""
+  };
+  obj.type = legend[i].Type; // "slot"
+  obj.raid = legend[i].Source; 
   if ( obj.raid !== "vaultofglass" 
     && obj.raid !== "kingsfall"
     && obj.raid !== "lastwish"
@@ -49,22 +150,89 @@ for (let i = 0; i < legend.length; ++i) {
     && obj.raid !== "deepstonecrypt"
     && obj.raid !== "vowofthedisciple"
     && obj.raid !== "rootofnightmares"
+    && obj.raid !== "ironbanner"
   ){
     obj.raid = ""; 
+    if (legend[i]["Seasonal Mod"] === "artifice") {
+      obj.raid = "artifice";
+    }
   }
-  obj.class = legend[i].Equippable
+  obj.class = legend[i].Equippable;
   switch(obj.class) {
     default:
       break;
     case "Warlock":
-      // compare stats and compile defecit/supplement strings and "spike"
-      let d = legend[i]["Discipline (Base)"];
-      let s = legend[i]["Strength (Base)"];
-      obj.spike = (d >= s) ? d : s;
+      buildDefecitsAndSupplementsWarlockAndTitan(
+        obj,
+        legend[i]["Resilience (Base)"],
+        legend[i]["Recovery (Base)"],
+        legend[i]["Discipline (Base)"],
+        legend[i]["Strength (Base)"],
+        w_res,
+        w_rec,
+        w_disc,
+        w_str
+      );
       break;
     case "Titan":
+      buildDefecitsAndSupplementsWarlockAndTitan(
+        obj,
+        legend[i]["Resilience (Base)"],
+        legend[i]["Recovery (Base)"],
+        legend[i]["Discipline (Base)"],
+        legend[i]["Strength (Base)"],
+        t_res,
+        t_rec,
+        t_disc,
+        t_str
+      );
       break;
     case "Hunter":
+      buildDefecitsAndSupplementsHunter(
+        obj,
+        legend[i]["Mobility (Base)"],
+        legend[i]["Resilience (Base)"],
+        legend[i]["Recovery (Base)"],
+        legend[i]["Discipline (Base)"],
+        h_mob,
+        h_res,
+        h_rec,
+        h_disc
+      );
       break;
   }
+  db[db.length] = obj;
 }
+
+console.log(db);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
